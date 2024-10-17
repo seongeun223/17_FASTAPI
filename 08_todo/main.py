55d2429bfbc955fe936daa36dfc451278d3851f8
seongeun223 <jklun080921@gmail.com>
2024-10-17 18:04:30 +0900

    test

--- 08_todo/main.py
+++ 08_todo/main.py
@@ -7,25 +7,28 @@ from sqlalchemy.orm import Session
 # FastAPI = 어플리케이션에서 Jinja2 템플릿 엔진을 사용하도록 설정
 templates = Jinja2Templates(directory="template")
 
-app = FastAPI();
+app = FastAPI()
 
 # 데이터베이스 테이블 생성
 models.Base.metadata.create_all(bind=engine)
 
+
 # 데이터베이스 세션
 def get_db():
-    db = session_local() # 호출될때마다 새로운 세션 객체 생성
+    db = session_local()  # 호출될때마다 새로운 세션 객체 생성
     try:
-        yield db # 데이터 베이스 세션 객체 반환
+        yield db  # 데이터 베이스 세션 객체 반환
     finally:
         db.close()
-        
+
 
 @app.get("/")
-async def home(request: Request, db : Session = Depends(get_db)):
-    
+async def home(request: Request, db: Session = Depends(get_db)):
+
     # 데이터베이스에서 Todo 모델을 가져온다. (id를 기준으로 내림차순정렬)
     todos = db.query(models.Todo).order_by(models.Todo.id.desc())
-    
+
     # 인덱스 템플릿 랜더링
-    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "todos": todos}
+    )
